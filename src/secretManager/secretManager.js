ae536313d4139cc94a47f029a6aca7ba534293b6
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-01-27 10:18:29 +0530

    Added port in secret manager

--- src/secretManager/secretManager.js
+++ src/secretManager/secretManager.js
@@ -1,7 +1,7 @@
 const authServerURLKey = "AUTH_SERVER_URL";
 const hashedSecretKey = "HASHED_SECRET";
 const jwtSecret = "JWT_SECRET";
-
+const port = "APPLICATION_PORT";
 function getSecret(key) {
     return process.env[key]
 }
@@ -14,4 +14,7 @@ function getHashedSecret() {
 function getJwtSecret() {
     return getSecret(jwtSecret);
 }
-module.exports = { getSecret, getAuthServerURL, getHashedSecret, getJwtSecret }
+function getApplicationPort() {
+    return parseInt(getSecret(APPLICATION_PORT));
+}
+module.exports = { getApplicationPort, getAuthServerURL, getHashedSecret, getJwtSecret }
