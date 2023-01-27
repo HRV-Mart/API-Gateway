8a7d886f809ef90af7db9e3a107eb03d5e342999
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-01-27 09:52:35 +0530

    Added secret manager

--- src/secretManager/secretManager.js
+++ src/secretManager/secretManager.js
@@ -1,4 +1,13 @@
+const authServerURLKey = "AUTH_SERVER_URL";
+const hashedSecretKey = "HASHED_SECRET";
+
 function getSecret(key) {
     return process.env[key]
 }
-module.exports = { getSecret }
+function getAuthServerURL() {
+    return getSecret(authServerURLKey)
+}
+function getHashedSecret() {
+    return getSecret
+}
+module.exports = { getSecret, getAuthServerURL, getHashedSecret }
