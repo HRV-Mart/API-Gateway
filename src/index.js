7aa1f37df518d26fa0da19cfffdcba602a74746a
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-01-27 09:53:31 +0530

    Expose auth route

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
+// Import libraries
 const express = require("express");
-const { getSecret } = require("./secretManager/secretManager");
 const { logError, logMessage } = require("./logging/logging");
+const cors = require("cors");
+const { json } = require("express");
+
+// Import routes
+const authRoutes = require("./routes/auth");
+
 
 const app = express();
-
 require('dotenv').config();
 
-const port = parseInt(getSecret("APPLICATION_PORT"));
+app.use(cors());
+app.use(json());
+
+app.use(authRoutes)
+
+const port = parseInt("3002");
 app.listen(port, (error) => {
     if (error) {
         logError(error)
