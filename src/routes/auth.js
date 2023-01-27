39a3023055f8aa96955af7f0990fa048bd115d07
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-01-27 10:14:53 +0530

    Added login and sign up route

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,6 +2,8 @@ const router = require("express").Router();
 const crypto = require('crypto');
 const { getHashedSecret, getAuthServerURL } = require("./../secretManager/secretManager");
 const { postRequest } = require("./../networking/networking");
+const { logMessage } = require("./../logging/logging");
+const { createJWT } = require("../middleware/middleware");
 // Login
 router.post("/login", async (req, res) => {
     const email = req.body.email;
@@ -16,15 +18,51 @@ router.post("/login", async (req, res) => {
             email: email,
             hashedPassword: hashedPassword
         };
-
-        const response = await postRequest(getAuthServerURL(), payload, {}, false);
-        res.status(response.status).send(response.data);
+        const header = {
+            "Content-Type": "application/json"
+        }
+        const path = `${getAuthServerURL()}/auth/login`;
+        const response = await postRequest(path, payload, header, false);
+        if (response.status == 200) {
+            const token = createJWT(email);
+            res.status(200).json({
+                jwt: token,
+                message: response.data
+            });
+        }
+        else {
+            res.status(response.status).send(response.data);
+        }
     }
     else {
-        res.status(500).send("Send Valid Email")
+        res.status(500).send("Send Valid Email");
     }
 });
 // Sign up
+router.post("/signup", async (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    if (verifyEmail(email)) {
+        const hashedPassword = crypto.createHash("sha256", getHashedSecret())
+            .update(password)
+            .digest("hex");
+
+        const payload = {
+            email: email,
+            hashedPassword: hashedPassword
+        };
+        const header = {
+            "Content-Type": "application/json"
+        }
+        const path = `${getAuthServerURL()}/auth/signup`;
+        const response = await postRequest(path, payload, header, false);
+        res.status(response.status).send(response.data);
+    }
+    else {
+        res.status(500).send("Send Valid Email");
+    }
+});
 // Email verifier
 function verifyEmail(email) {
     if (email.includes('@')) {
