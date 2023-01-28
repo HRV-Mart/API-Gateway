c6af7caaaded80a9a6c45728e361bc4f93bd9392
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-01-28 12:12:37 +0530

    created auth service file

---
+++ src/service/authService.js
@@ -0,0 +1,15 @@
+const { postRequest, putRequest } = require("./../networking/networking");
+function signUp(email, password) {
+
+}
+function login(email, password) {
+
+}
+function updatePassword(email, password) {
+
+}
+module.exports = {
+    signUp,
+    login,
+    updatePassword
+}
