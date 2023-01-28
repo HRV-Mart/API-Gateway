7da09031c8700bd142e1eb728279f811a651915a
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-01-28 13:06:14 +0530

    Added token service for maintaining tokens

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+
+const auth = require("./router/auth");
+const { getApplicationPort } = require("./secretManager/secretManager");
+const { logError, logMessage } = require("./logging/logging");
+
+const app = express();
+require("dotenv").config();
+
+app.use(express.json());
+app.use(cors());
+app.use("", auth);
+
+// const port = getApplicationPort();
+const port = 3002;
+app.listen(port, (error) => {
+    if (error) {
+        logError(error);
+    }
+    else {
+        logMessage(`Application is running at port ${port}`);
+    }
+});
