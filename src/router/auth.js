0c0742a6df0d2573e66ce28caf8614e7a895367f
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-01-28 13:06:40 +0530

    Added login end-point

--- src/router/auth.js
+++ src/router/auth.js
@@ -1,12 +1,48 @@
 const router = require("express").Router();
-const { signUp } = require("./../service/authService");
+const { createJWT } = require("../service/tokenService");
+const { signUp, login } = require("./../service/authService");
 
 // Sign Up
 router.post('/signup', async (req, res) => {
     try {
-        const response = await signUp(req.body.email, req.body.password);
-        res.status(response.status).send(response.data);
-        // Create User if status = 200.
+        const email = req.body.email;
+        const password = req.body.password;
+
+        const response = await signUp(email, password);
+        if (response.status == 200) {
+            // Create User if status = 200.
+            const token = createJWT(email);
+            res.status(response.status).json({
+                token: token,
+                message: response.data
+            });
+        }
+        else {
+            res.status(response.status).send(response.data);
+        }
+    }
+    catch (error) {
+        res.status(500).send("Something Went Wrong!!")
+    }
+});
+// Login
+router.post('/login', async (req, res) => {
+    try {
+        const email = req.body.email;
+        const password = req.body.password;
+
+        const response = await login(email, password);
+        if (response.status == 200) {
+            // Create User if status = 200.
+            const token = createJWT(email);
+            res.status(response.status).json({
+                token: token,
+                message: response.data
+            });
+        }
+        else {
+            res.status(response.status).send(response.data);
+        }
     }
     catch (error) {
         res.status(500).send("Something Went Wrong!!")
