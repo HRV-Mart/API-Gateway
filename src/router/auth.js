e4a1d336297bf1aa21cb30f90c020cbced09f967
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-01-29 08:53:46 +0530

    Added create user feature while sign up

--- src/router/auth.js
+++ src/router/auth.js
@@ -1,24 +1,42 @@
 const router = require("express").Router();
+const { logError } = require("../logging/logging");
 const { createJWT } = require("../service/tokenService");
 const { signUp, login } = require("./../service/authService");
+const { createUser } = require("./../service/userService");
 
 // Sign Up
 router.post('/signup', async (req, res) => {
     try {
         const email = req.body.email;
         const password = req.body.password;
-
-        const response = await signUp(email, password);
-        if (response.status == 200) {
+        const name = req.body.name
+        if (email == undefined || password == undefined || name == undefined) {
+            res.status(404).send("Not Found");
+            return;
+        }
+        const authResponse = await signUp(email, password);
+        if (authResponse.status == 200) {
             // Create User if status = 200.
-            const token = createJWT(email);
-            res.status(response.status).json({
-                token: token,
-                message: response.data
-            });
+            const userResponse = await createUser(name, email);
+            if (userResponse.status == 200) {
+                const token = createJWT(email);
+                res.status(authResponse.status).json({
+                    token: token,
+                    message: authResponse.data
+                });
+            }
+            else {
+                logError({
+                    userId: email,
+                    message: "Unable to create user after creating auth",
+                    data: userResponse
+                });
+                res.status(500).send("Something went wrong");
+            }
+
         }
         else {
-            res.status(response.status).send(response.data);
+            res.status(authResponse.status).send(authResponse.data);
         }
     }
     catch (error) {
