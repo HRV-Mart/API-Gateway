727b1cb8335540a7e3f689c509e6974a83a2221e
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-02-01 11:29:46 +0530

    Added default secret

--- src/secretManager/secretManager.js
+++ src/secretManager/secretManager.js
@@ -7,10 +7,10 @@ const jwtSecret = "JWT_SECRET";
 const APPLICATION_PORT = "APPLICATION_PORT";
 
 function getSecret(key) {
-    return process.env[key]
+    return process.env[key] || getDefaultSecret(key);
 }
 function getAuthServerURL() {
-    return getSecret(authServerURLKey)
+    return getSecret(authServerURLKey);
 }
 function getUserServerURL() {
     return getSecret(userServerURLKey);
@@ -19,17 +19,26 @@ function getProductServerURL() {
     return getSecret(productServerURLKey);
 }
 function getHashedSecret() {
-    return getSecret(hashedSecretKey)
+    return getSecret(hashedSecretKey);
 }
 function getJwtSecret() {
     return getSecret(jwtSecret);
 }
 function getApplicationPort() {
-    return parseInt(getSecret(APPLICATION_PORT) || getDefaultPort());
+    return parseInt(getSecret(APPLICATION_PORT);
 }
 function getDefaultPort() {
     logError("Unable to find application port in secret");
     return 3002;
+}
+function getDefaultSecret(key) {
+    logError(`Unable to find ${key} secret`);
+    if (key == APPLICATION_PORT) {
+        return 3002;
+    }
+    else {
+        return "SECRET";
+    }
 }
 module.exports = {
     getApplicationPort,
