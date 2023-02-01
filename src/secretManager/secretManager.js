cdc057b7445cbe3e2c7061f07f40588d3a9f4552
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-02-01 11:32:56 +0530

    Default secret manager (#14)
    * Added default port
    
    * Added default secret

--- src/secretManager/secretManager.js
+++ src/secretManager/secretManager.js
@@ -1,3 +1,4 @@
+const { logError } = require("./../logging/logging");
 const authServerURLKey = "AUTH_SERVER_URL";
 const userServerURLKey = "USER_SERVER_URL";
 const productServerURLKey = "PRODUCT_SERVER_URL";
@@ -6,10 +7,10 @@ const jwtSecret = "JWT_SECRET";
 const APPLICATION_PORT = "APPLICATION_PORT";
 
 function getSecret(key) {
-    return process.env[key]
+    return process.env[key] || getDefaultSecret(key);
 }
 function getAuthServerURL() {
-    return getSecret(authServerURLKey)
+    return getSecret(authServerURLKey);
 }
 function getUserServerURL() {
     return getSecret(userServerURLKey);
@@ -18,13 +19,26 @@ function getProductServerURL() {
     return getSecret(productServerURLKey);
 }
 function getHashedSecret() {
-    return getSecret(hashedSecretKey)
+    return getSecret(hashedSecretKey);
 }
 function getJwtSecret() {
     return getSecret(jwtSecret);
 }
 function getApplicationPort() {
     return parseInt(getSecret(APPLICATION_PORT));
+}
+function getDefaultPort() {
+    logError("Unable to find application port in secret");
+    return 3002;
+}
+function getDefaultSecret(key) {
+    logError(`Unable to find ${key} secret`);
+    if (key == APPLICATION_PORT) {
+        return 3002;
+    }
+    else {
+        return "SECRET";
+    }
 }
 module.exports = {
     getApplicationPort,
