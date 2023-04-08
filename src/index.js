353122a0157f4c63cbe2be3a45b79e85f731fc6b
Harsh3305 <harshverma3305@gmail.com>
2023-04-08 09:56:58 +0530

    Added order route in application

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const auth = require("./router/auth");
 const product = require("./router/product");
 const cart = require("./router/cart");
+const order = require("./router/order");
 
 const { getApplicationPort } = require("./secretManager/secretManager");
 const { logError, logMessage } = require("./logging/logging");
@@ -15,7 +16,8 @@ app.use(express.json());
 app.use(cors());
 app.use("", auth);
 app.use("/product", product);
-app.use("/cart", cart)
+app.use("/cart", cart);
+app.use("/order", order);
 
 // const port = getApplicationPort();
 const port = 3002;
