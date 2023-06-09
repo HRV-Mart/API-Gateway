07c0ac42e46d5ee15c089ac01e111345aeabc0f0
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-06-09 14:51:16 +0530

    Change auth endpoint (#41)
    * Update auth service
    
    * Updated auth endpoint. Remove sign up route

--- src/service/authService.js
+++ src/service/authService.js
@@ -1,47 +1,11 @@
-const { postRequest, putRequest } = require("./../networking/networking");
-const { getAuthServerURL, getHashedSecret } = require("./../secretManager/secretManager");
-const crypto = require("crypto");
-async function signUp(email, password, name) {
+const { getRequest } = require("./../networking/networking");
+const { getAuthServerURL } = require("./../secretManager/secretManager");
 
-    const header = getHeader()
-    const body = {
-        authDetail: getBody(email, password),
-        userDetail: {
-            emailId: email,
-            name: name
-        }
-    };
-    const response = await postRequest(`${getAuthServerURL()}/signup`, body, header, false);
-    return response;
+async function login(jwt) {
+    const response = await getRequest(`${getAuthServerURL()}?jwt=${jwt}`, {}, true)
+    return response
 }
-async function login(email, password) {
-    const header = getHeader()
-    const body = getBody(email, password);
-    const response = await postRequest(`${getAuthServerURL()}/login`, body, header, false);
-    return response;
-}
-async function updatePassword(email, password) {
 
-}
-function generateHashedPassword(password) {
-    const hashedPassword = crypto.createHash("sha256", getHashedSecret())
-        .update(password)
-        .digest("hex");
-    return hashedPassword;
-}
-function getBody(email, password) {
-    return {
-        email: email,
-        hashedPassword: generateHashedPassword(password)
-    };
-}
-function getHeader() {
-    return {
-        "Content-Type": "application/json"
-    };
-}
 module.exports = {
-    signUp,
-    login,
-    updatePassword
+    login
 }
